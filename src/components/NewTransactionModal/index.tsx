30c9c15ced9305370abfe352191bdb6664673120
LuanLeiteLeão <luanleiteraio@gmail.com>
2022-05-21 07:38:25 -0300

    :construction worker: ser form structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,28 @@
 import Modal from 'react-modal'
+import { ContainerForm } from './styles'
 
-interface NewTransactionModalProps{
-    onOpen:boolean
-    onRequestClose:()=>void
+interface NewTransactionModalProps {
+    onOpen: boolean
+    onRequestClose: () => void
 }
 
-export function NewTransactionModal({onOpen, onRequestClose}:NewTransactionModalProps){
+export function NewTransactionModal({ onOpen, onRequestClose }: NewTransactionModalProps) {
     return (
         <Modal
-          isOpen={onOpen}
-          onRequestClose={onRequestClose}
+            isOpen={onOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-          <h1>Cadastrar Transação</h1>
+            <ContainerForm>
+                <h1>Cadastrar Transação</h1>
+                <input placeholder="Título" />
+                <input type="number" placeholder="Valor" />
+                <input placeholder="Categoria" />
+
+                <button type="submit" />
+            </ContainerForm>
+
         </Modal>
-      )
+    )
 }
